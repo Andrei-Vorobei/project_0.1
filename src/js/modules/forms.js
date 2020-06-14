c7fffb09dcb91e401f3d3713f73cc613aed5b037
Andrei <marchenko_ad@mail.ru>
2020-06-14 20:35:26 +0300

    added calc-modals

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,11 @@
-const forms = () => {
+import checkNumInputs from './checkNumInputs';
+
+const forms = (state) => {
 	const form = document.querySelectorAll('form'),
 			inputs = document.querySelectorAll('input'),
-			phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+			windows = document.querySelectorAll('[data-modal]');
 
-	phoneInputs.forEach(item => {
-		item.addEventListener('input', () => {
-			item.value = item.value.replace(/\D/, '');
-		});
-	});
+	checkNumInputs('input[name="user_phone"]');
 
 	const message = {
 		loading: 'Загрузка...',
@@ -40,6 +38,11 @@ const forms = () => {
 			item.appendChild(statusMessage);
 
 			const formData = new FormData(item);
+			if (item.getAttribute('data-calc') === "end") {
+				for (let key in state) {
+					formData.append(key, state[key]);
+				}
+			}
 
 			postData('assets/server.php', formData)
 			.then(res => {
@@ -49,8 +52,14 @@ const forms = () => {
 			.catch(() => statusMessage.textContent = message.failure)
 			.finally(() => {
 				clearInputs();
+				for (let key in state) {
+					delete state[key];
+				}
 				setTimeout(() => {
 					statusMessage.remove();
+					windows.forEach(item => {
+						item.style.display = 'none';
+					});
 				}, 5000);
 			});
 		});
