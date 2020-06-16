4a1d71c488cb3adfc3bc74852fec1b59e4a74a7d
Andrei <marchenko_ad@mail.ru>
2020-06-16 10:17:58 +0300

    project ended

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = (state) => {
 		const trigger = document.querySelectorAll(triggerSelector),
 				modal = document.querySelector(modalSelector),
 				close = document.querySelector(closeSelector),
-				windows = document.querySelectorAll('[data-modal]');
+				windows = document.querySelectorAll('[data-modal]'),
+				scroll = calcScroll();
 
 		trigger.forEach(item => {
 			let event = item.addEventListener('click', (e) => {
@@ -29,6 +30,7 @@ const modals = (state) => {
 	
 				modal.style.display = "block";
 				document.body.style.overflow = "hidden";
+				document.body.style.marginRight = `${scroll}px`;
 				// document.body.classList.add('modal-open');
 			});
 		});
@@ -40,6 +42,7 @@ const modals = (state) => {
 
 			modal.style.display = "none";
 			document.body.style.overflow = "";
+			document.body.style.marginRight = '0';
 			// document.body.classList.remove('modal-open');
 		});
 
@@ -51,6 +54,7 @@ const modals = (state) => {
 
 				modal.style.display = "none";
 				document.body.style.overflow = "";
+				document.body.style.marginRight = '0';
 				// document.body.classList.remove('modal-open');
 			}
 		});
@@ -61,6 +65,23 @@ const modals = (state) => {
 			document.querySelector(selector).style.display = 'block';
 			document.body.style.overflow = "hidden";
 		}, time);
+	}
+
+	function calcScroll() {
+		let div = document.createElement('div');
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflowY = 'scroll';
+		div.style.visibility = 'hidden';
+
+		document.body.appendChild(div);
+
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+
+		div.remove();
+
+		console.log(scrollWidth);
+		return scrollWidth;
 	}
 	
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
